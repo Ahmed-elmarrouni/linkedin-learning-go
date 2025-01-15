3ebf42db538aff95c2ac1c9bc4d5a28c86f850e9
Ahmed El marrouni <ahmedelmarrouni1@gmail.com>
2025-01-15 17:53:38 +0100

    Fix some issues & correct some errors

--- practice/main.go
+++ practice/main.go
@@ -19,12 +19,17 @@ func main() {
 	fmt.Println(carsBrand[3])
 	fmt.Println("Numbers of cars", len(carsBrand))
 
-	// if i have unlimited number of elements
+	// if i have unlimited number of elements "we called slices"
 	carsModel := []string{"Corolla", "Focus", "Camaro"}
 	fmt.Println(carsModel)
 
-	// add new item to the array
+	// add new item to the slice
 	carsModel = append(carsModel, "Dacia")
 	fmt.Println(carsModel)
+
+	// create a slice
+	var colors = []string{"Red", "Green", "Blue"}
+	colors = append(colors, "Yellow", "Purple")
+	fmt.Println(colors)
 
 }
