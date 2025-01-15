b8c36241dd9e2a28ef49747fb238f8c6e7ddbbfc
Ahmed El marrouni <ahmedelmarrouni1@gmail.com>
2025-01-15 14:14:25 +0100

    Get input from the console

--- practice/main.go
+++ practice/main.go
@@ -1,13 +1,20 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 )
 
 func main() {
+	// reader := bufio.NewReader(os.Stdin)
+	// fmt.Print("Enter text: ")
+	// os.Stdout.Sync()
+	// input, _ := reader.ReadString('\n')
+	// fmt.Println("You entered:", input)
 
-	var aString string = "This is Go!"
-	fmt.Println(aString)
-	fmt.Printf("The variable's type is %T\n", aString)
-
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Enter ur full Name: ")
+	Full_Name, _ := reader.ReadString('\n')
+	fmt.Println("Ur Full name is:", Full_Name)
 }
