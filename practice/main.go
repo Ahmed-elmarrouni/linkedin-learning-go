06815cb14e3d193d3ab8ab4987d68172379371b2
Ahmed El marrouni <ahmedelmarrouni1@gmail.com>
2025-01-15 22:09:10 +0100

    Discover Switch Statement

--- practice/main.go
+++ practice/main.go
@@ -10,4 +10,19 @@ func main() {
 	rand.Seed(time.Now().Unix())
 	dow := rand.Intn(7) + 1
 	fmt.Println("Day", dow)
+
+	var result string
+	switch dow {
+	case 1:
+		result = "It's Sunday!"
+	case 2:
+		result = "It's Monday!"
+	case 3:
+		result = "It's Tuesday!"
+	default:
+		result = "It's a weekday!"
+	}
+
+	fmt.Println("Day", result)
+
 }
