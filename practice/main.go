0086ae04f4a69b3689d8bac980b54cc2a80277da
Ahmed El marrouni <ahmedelmarrouni1@gmail.com>
2025-01-15 17:12:29 +0100

    Discover pointers

--- practice/main.go
+++ practice/main.go
@@ -5,5 +5,17 @@ import (
 )
 
 func main() {
-	fmt.Println("Pointers")
+
+	anInt := 42
+	var p = &anInt
+	fmt.Println("value of p is: ", *p)
+
+	value1 := 50
+	pointer1 := &value1
+	fmt.Println("value of pointer1 is: ", *pointer1)
+
+	*pointer1 = *pointer1 / 15
+	fmt.Println("value of pointer1 is: ", *pointer1)
+	fmt.Println("value of value1 is: ", value1)
+
 }
