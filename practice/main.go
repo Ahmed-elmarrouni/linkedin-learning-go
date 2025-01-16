d4714686408af9ce813557ee86d248c1efcafcc6
Ahmed El marrouni <ahmedelmarrouni1@gmail.com>
2025-01-16 21:56:43 +0100

    Define and call func's

--- practice/main.go
+++ practice/main.go
@@ -5,5 +5,29 @@ import (
 )
 
 func main() {
-	fmt.Println("Functions")
+	doSomething()
+
+	multiSum, multiCount := addAllValues(7, 8, 10)
+	fmt.Println("Sum of all values is: ", multiSum)
+	fmt.Println("Count of all values is: ", multiCount)
+}
+
+func doSomething() {
+	fmt.Println("Doing something")
+	sumV := addValues(8, 2)
+	fmt.Println("Sum of Va and V2 is: ", sumV)
+}
+
+func addValues(valueA int, valueB int) int {
+
+	return valueA + valueB
+}
+
+func addAllValues(value ...int) (int, int) {
+	total := 0
+	for _, val := range value {
+		total += val
+	}
+
+	return total, len(value)
 }
